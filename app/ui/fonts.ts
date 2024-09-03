fc86c3359256ca0b6089625bda722e7d068555b3
Volodymyr <vovoset82@gmail.com>
2024-09-03 12:19:15 +0300

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Roboto,Inter,Lusitana } from 'next/font/google';
+ 
+export const inter =Inter({subsets: ['latin']});
+export const roboto =Roboto({weight:["100","700"],subsets: ['latin']});
+ 
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
