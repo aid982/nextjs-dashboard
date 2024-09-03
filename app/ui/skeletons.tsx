2ad993a3e40fcaf716277a7b2684ee66602c1f41
Volodymyr <vovoset82@gmail.com>
2024-09-03 14:50:39 +0300

    Adding dynamic fetching ,Suspense bounderies

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -16,16 +16,17 @@ export function CardSkeleton() {
       </div>
     </div>
   );
+  
 }
 
 export function CardsSkeleton() {
   return (
-    <>
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-    </>
+    </div>
   );
 }
 
